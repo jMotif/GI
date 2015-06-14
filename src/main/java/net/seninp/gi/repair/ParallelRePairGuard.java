1f4e62b8f086dc4fec9f25f87045e8e888e4471f
seninp <seninp@gmail.com>
2015-06-14 06:38:46 +0200

    moving gi code

---
+++ src/main/java/net/seninp/gi/repair/ParallelRePairGuard.java
@@ -0,0 +1,22 @@
+package net.seninp.gi.repair;
+
+
+public class ParallelRePairGuard extends Symbol {
+
+  protected ParallelRePairRule rule;
+
+  public ParallelRePairGuard(ParallelRePairRule r) {
+    super();
+    this.rule = r;
+    r.setGuard(this);
+  }
+
+  public String toString() {
+    return this.rule.toString();
+  }
+
+  public boolean isGuard() {
+    return true;
+  }
+
+}
