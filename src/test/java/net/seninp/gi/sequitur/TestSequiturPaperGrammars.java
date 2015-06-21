6b7d9cba55b058085140956265cb79ebc67be9f3
seninp <seninp@gmail.com>
2015-06-21 16:42:24 +0200

    adding the "deepest hierarchy" test

--- src/test/java/net/seninp/gi/sequitur/TestSequiturPaperGrammars.java
+++ src/test/java/net/seninp/gi/sequitur/TestSequiturPaperGrammars.java
@@ -1,5 +1,6 @@
 package net.seninp.gi.sequitur;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import net.seninp.gi.GrammarRules;
@@ -16,6 +17,27 @@ public class TestSequiturPaperGrammars {
   private static final String TEST2_R0 = "R1 R1";
   private static final String TEST2_R1 = "a R2 d R2";
   private static final String TEST2_R2 = "b c";
+
+  private static final String TEST3_STRING = "a b a b c a b c d a b c d e a b c d e f";
+  private static final String TEST3_R0 = "R1 R2 R3 R4 R4 f";
+  private static final String TEST3_R4 = "R3 e";
+
+  @Test
+  public void test3() {
+    try {
+      SAXRule r = SequiturFactory.runSequitur(TEST3_STRING);
+      GrammarRules rules = r.toGrammarRulesData();
+
+      assertEquals("test hierarchy", 5, rules.size());
+
+      assertTrue("test r0", TEST3_R0.equals(rules.get(0).getRuleString().trim()));
+
+      assertTrue("test r1", TEST3_R4.equals(rules.get(4).getRuleString().trim()));
+    }
+    catch (Exception e) {
+      fail("Exception shouldnt be thrown: " + StackTrace.toString(e));
+    }
+  }
 
   @Test
   public void test2() {
