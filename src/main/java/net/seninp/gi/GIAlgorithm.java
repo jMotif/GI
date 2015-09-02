ca3cf61e78baae13936e94798221e2eb721f40c3
Pavel Senin <seninp@gmail.com>
2015-09-02 18:01:29 +0200

    adding the algorithm selector

--- src/main/java/net/seninp/gi/GIAlgorithm.java
+++ src/main/java/net/seninp/gi/GIAlgorithm.java
@@ -1,6 +1,5 @@
 package net.seninp.gi;
 
-
 /**
  * GI algorithm selector.
  * 
@@ -27,6 +26,53 @@ public enum GIAlgorithm {
   }
 
   /**
+   * Get the alg index.
+   * 
+   * @return alg index.
+   */
+  public int toAlgIndex() {
+    return this.algIndex;
+  }
+
+  /**
+   * Parse the numerical value into an instance.
+   * 
+   * @param value the string value.
+   * @return new instance.
+   */
+  public static GIAlgorithm fromValue(int value) {
+    if (0 == value) {
+      return GIAlgorithm.SEQUITUR;
+    }
+    else if (1 == value) {
+      return GIAlgorithm.REPAIR;
+    }
+    else {
+      throw new RuntimeException("Unknown index:" + value);
+    }
+
+  }
+
+  /**
+   * Parse the string value into an instance.
+   * 
+   * @param value the string value.
+   * @return new instance.
+   */
+  public static GIAlgorithm fromValue(String value) {
+    if ("sequitur".equalsIgnoreCase(value) || "s".equalsIgnoreCase(value)) {
+      return GIAlgorithm.SEQUITUR;
+    }
+    else if ("repair".equalsIgnoreCase(value) || "re-pair".equalsIgnoreCase(value)
+        || "r".equalsIgnoreCase(value)) {
+      return GIAlgorithm.REPAIR;
+    }
+    else {
+      throw new RuntimeException("Unknown index:" + value);
+    }
+  }
+
+  /**
    * {@inheritDoc}
    */
   public String toString() {
@@ -37,25 +83,6 @@ public enum GIAlgorithm {
       return "Re-Pair";
     default:
       throw new RuntimeException("Unknown index:" + this.algIndex);
-    }
-  }
-
-  /**
-   * Parse the string value into an instance.
-   * 
-   * @param value the string value.
-   * @return new instance.
-   */
-  public static GIAlgorithm fromString(String value) {
-    if ("sequitur".equalsIgnoreCase(value) || "s".equalsIgnoreCase(value)) {
-      return GIAlgorithm.SEQUITUR;
-    }
-    else if ("repair".equalsIgnoreCase(value) || "re-pair".equalsIgnoreCase(value)
-        || "r".equalsIgnoreCase(value)) {
-      return GIAlgorithm.REPAIR;
-    }
-    else {
-      throw new RuntimeException("Unknown index:" + value);
     }
   }
 }
