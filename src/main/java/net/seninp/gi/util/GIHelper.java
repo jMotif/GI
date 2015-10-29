a543d974d6377303eb608456e534577f99a933cb
Pavel Senin <seninp@gmail.com>
2015-10-29 15:50:38 +0100

    bumping the SAX version and moving the Grammar CLI code

--- src/main/java/net/seninp/gi/util/GIHelper.java
+++ src/main/java/net/seninp/gi/util/GIHelper.java
@@ -1,5 +1,9 @@
 package net.seninp.gi.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import net.seninp.gi.RuleInterval;
+
 /**
  * I use this for temporal fixtures.
  * 
@@ -29,4 +33,30 @@ public class GIHelper {
     return sum / (double) values.length;
 
   }
+
+  /**
+   * Run a quick scan along the time series coverage to find a zeroed intervals.
+   * 
+   * @param coverageArray the coverage to analyze.
+   * @return set of zeroed intervals (if found).
+   */
+  public static List<RuleInterval> getZeroIntervals(int[] coverageArray) {
+    ArrayList<RuleInterval> res = new ArrayList<RuleInterval>();
+    int start = -1;
+    boolean inInterval = false;
+    int intervalsCounter = -1;
+    for (int i = 0; i < coverageArray.length; i++) {
+      if (0 == coverageArray[i] && !inInterval) {
+        start = i;
+        inInterval = true;
+      }
+      if (coverageArray[i] > 0 && inInterval) {
+        res.add(new RuleInterval(intervalsCounter, start, i, 0));
+        inInterval = false;
+        intervalsCounter--;
+      }
+    }
+    return res;
+  }
+
 }
