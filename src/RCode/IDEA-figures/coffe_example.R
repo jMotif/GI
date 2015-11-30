06c4423fba71707b773f69f1aa08852803cf9eb2
seninp <seninp@gmail.com>
2015-11-30 09:08:18 +0100

    working on the figures

--- src/RCode/IDEA-figures/coffe_example.R
+++ src/RCode/IDEA-figures/coffe_example.R
@@ -8,12 +8,17 @@ library(Cairo)
 library(reshape)
 library(plyr)
 library(dplyr)
+# The palette with black:
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", 
+                "#D55E00", "#CC79A7")
+
 #
 cd <- read.table("../resources/Coffee/Coffee_TEST",as.is=T)[12,-1]
 dd <- data.frame(x = 1:length(unlist(cd)), y = unlist(cd))
-p = ggplot(dd, aes(x = x, y = y)) + geom_line(color = "cornflowerblue", lwd=1) + 
-  theme_bw() + ggtitle(paste("An instant Coffee spectrogram represented as a time series\n",
-    "and subsequence extraction via sliding window illustration")) +
+p = ggplot(dd, aes(x = x, y = y)) + geom_line(color = "cornflowerblue", lwd=0.7) + 
+  scale_y_continuous(limits=c(-1,35)) +
+  theme_bw() + ggtitle(paste("An illustration of time series subsequence extraction\n",
+                      "via sliding window")) +
   theme(axis.title=element_blank(), legend.position="none")
 p
 #
@@ -25,8 +30,9 @@ for (i in c(15,20)) {
 }
 cd_df = as.data.frame(cd_df)
 cd_df$window = paste(c(1:3))
+cd_df$color=c("brown1","brown1","brown")
 row.names(cd_df)=NULL
-cd_m = melt(cd_df, id.var=c("window"))
+cd_m = melt(cd_df, id.var=c("window","color"))
 vv=(c(10,15,20))
 dv = vv
 for(i in c(1:step)){
@@ -35,39 +41,45 @@ for(i in c(1:step)){
 cd_m$variable = dv
 #
 sg=data.frame(x=c(10,15,20,c(10,15,20)+step),
-              y=c(dd$y[c(10,15,20,c(10,15,20)+step)])-c(0,2,4,0,2,4),
-              yend=rep(0,6),line=c(1,2,3))
+              y=c(dd$y[c(10,15,20,c(10,15,20)+step)]),
+              yend=c(2,1,0,2,1,0))
+sf=data.frame(x=c(10,15,20),xend=c(10,15,20)+step,
+              y=c(2,1,0),yend=c(2,1,0),color=c("brown1","brown1","brown"))
 
-
-p = p + 
-  geom_segment(data=sg,aes(x=x,y=y,xend=x,yend=yend), col="brown", lty=3, lwd=0.5) +
-  geom_path(data=cd_m,aes(x=variable,y=value,col=window,group=window),lwd=1)
+p = 
+p + geom_segment(data=sg,aes(x=x,y=y,xend=x,yend=yend), col="brown", lty=3, lwd=0.5) +
+  geom_path(data=cd_m,aes(x=variable,y=value,group=window),color=cd_m$color,lwd=0.8) +
+  geom_segment(data=sf,aes(x=x,y=y,xend=xend,yend=yend), color=sf$color, lwd=1)
+p
+#
+#
 #
 #
 #
 #
 cd <- read.table("../resources/Coffee/Coffee_TEST",as.is=T)[12,-1][70:170]
 dd <- data.frame(x = 1:length(unlist(cd)), y = znorm(unlist(cd)))
-p2 = ggplot(dd, aes(x = x, y = y)) + geom_line(color = "cornflowerblue", lwd=1) + 
-  theme_bw() + ggtitle(paste("An example of SAX transform: a time series\n",
-                             "of length 100 converted into 8 letters")) +
+p2 = ggplot(dd, aes(x = x, y = y)) + geom_line(color = "cornflowerblue", lwd=0.7) + 
+  theme_bw() + ggtitle(paste("An example of SAX transform: a time series of 100 points\n",
+                "converted into 5 letters, 'cabbc'")) +
   scale_x_continuous(limits=c(-40,105)) + scale_y_continuous(limits=c(-2,2)) +
   theme(axis.title=element_blank(), legend.position="none")
 p2
 #
-segments = 8
+segments = 5
 inc = length(dd$y)/segments # there shall be 15 segments
 breaks = seq(1-0.5,length(dd$y)+0.5,by = inc)
-p2 + geom_vline(xintercept=breaks, lty=3, color="red")
+p2 + geom_vline(xintercept=breaks, lty=3, color="red", lwd=0.5)
 #
 paa_points = data.frame(paa = jmotif::paa(dd$y, segments), 
                  paa_centers = (breaks + (inc / 2))[1:segments])
 paa_segments = data.frame(x=breaks[1:segments],y=paa_points$paa,
   xend=breaks[2:(segments+1)],yend=paa_points$paa,
-  color=c("blue","red","green","green","red","green","blue","red"))
-p2 = p2 + geom_vline(xintercept=breaks, lty=3, color="red") + 
-  geom_point(data=paa_points, aes(x=paa_centers, y=paa), pch=9, cex=2.7, col="brown") +
- geom_segment(data=paa_segments, aes(x=x,y=y,xend=xend,yend=yend, color=color),lwd=3)
+  color=c(cbbPalette[4],cbbPalette[6],cbbPalette[7],cbbPalette[7],cbbPalette[4]))
+p2 = p2 + geom_vline(xintercept=breaks, lty=3, color="red",lwd=0.5) + 
+  geom_point(data=paa_points, aes(x=paa_centers, y=paa), pch=9, cex=3.7, color=paa_segments$color) +
+ geom_segment(data=paa_segments, aes(x=x,y=y,xend=xend,yend=yend), 
+              color=paa_segments$color,lwd=1)
 p2
 bell = data.frame(y = seq(-2,2, length=100))
 bell$x= dnorm(bell$y, mean=0, sd=1)
@@ -75,22 +87,24 @@ bell=arrange(bell,y)
 bell$order = 1:100
 cuts = data.frame(y=alphabet_to_cuts(3)[2:3])
 labels=data.frame(label=c("a","b","c"),x=rep(-12,3),y=c(-1,0,1))
-p2 = p2 + geom_path(data=bell,aes(x=x*60-40,y=y),col="magenta",order=order,lwd=2) +
-  geom_hline(data=cuts,aes(yintercept=y), lty=2, lwd=0.8, col="magenta") +
-  geom_text(data=labels,aes(x=x,y=y,label=label),color="magenta")
+p2 = p2 + geom_path(data=bell,aes(x=x*50-40,y=y),col="magenta",order=order,lwd=1.2) +
+  geom_hline(data=cuts,aes(yintercept=y), lty=5, lwd=0.8, col="magenta") +
+  geom_text(data=labels,aes(x=x,y=y,label=label),
+            color=c(cbbPalette[6],cbbPalette[7],cbbPalette[4]), size=8)
+p2
 #
 #
 #
 cd <- read.table("../resources/Coffee/Coffee_TEST",as.is=T)[12,-1][70:170]
 dd <- data.frame(x = 1:length(unlist(cd)), y = znorm(unlist(cd)))
-p3 = ggplot(dd, aes(x = x, y = y)) + geom_line(color = "cornflowerblue", lwd=1) + 
-  theme_bw() + ggtitle(paste("An example of SAX transform: a time series\n",
-                             "of length 100 converted into 8 letters")) +
+p3 = ggplot(dd, aes(x = x, y = y)) + geom_line(color = "cornflowerblue", lwd=0.7) + 
+  theme_bw() + ggtitle(paste("An example of the PAA approximation error computation as\n",
+    "the sum of distances between points and their PAA values")) +
   scale_x_continuous(limits=c(0,105)) + scale_y_continuous(limits=c(-2,2)) +
   theme(axis.title=element_blank(), legend.position="none")
 p3
 #
-segments = 8
+segments = 5
 inc = length(dd$y)/segments # there shall be 15 segments
 breaks = seq(1-0.5,length(dd$y)+0.5,by = inc)
 p3 + geom_vline(xintercept=breaks, lty=3, color="red")
@@ -99,26 +113,80 @@ paa_points = data.frame(paa = jmotif::paa(dd$y, segments),
                         paa_centers = (breaks + (inc / 2))[1:segments])
 paa_segments = data.frame(x=breaks[1:segments],y=paa_points$paa,
                           xend=breaks[2:(segments+1)],yend=paa_points$paa)
-p3 = p3 + geom_vline(xintercept=breaks, lty=3, color="red") + 
+p3 = p3 + geom_vline(xintercept=breaks, lty=3, color="red",lwd=0.5) + 
   geom_point(data=paa_points, aes(x=paa_centers, y=paa), pch=9, cex=2.7, col="brown") +
-  geom_segment(data=paa_segments, aes(x=x,y=y,xend=xend,yend=yend), color="brown",lwd=2)
+  geom_segment(data=paa_segments, aes(x=x,y=y,xend=xend,yend=yend), color="brown",lwd=1)
 p3
-
+#
+x1 <- filter(dd, x<=breaks[2])$x
 df=data.frame(
-  y=c(paa_points$paa[1], filter(dd,y>paa_points$paa[1],x<20)$y),
-  x=c(0,filter(dd,y>paa_points$paa[1],x<20)$x)
-)
-p3=p3+geom_polygon(data=df)
+  x=x1, xend=x1, y=dd$y[x1], yend=rep(paa_points$paa[1],length(x1)))
+p3=p3+geom_segment(data=df,aes(x=x,y=y,yend=yend,xend=xend),col="cyan")
+#
+x2 <- filter(dd, x>breaks[2], x<=breaks[3])$x
 df=data.frame(
-  y=c(paa_points$paa[1], filter(dd,y<paa_points$paa[1],x<breaks[2])$y,paa_points$paa[1]),
-  x=c(paa_points$paa_centers[1],filter(dd,y<paa_points$paa[1],x<=breaks[2])$x,breaks[2])
-)
-p3+geom_polygon(data=df)
+  x=x2, xend=x2, y=dd$y[x2], yend=rep(paa_points$paa[2],length(x2)))
+p3=p3+geom_segment(data=df,aes(x=x,y=y,yend=yend,xend=xend),col="cyan")
+#
+x3 <- filter(dd, x>breaks[3], x<=breaks[4])$x
+df=data.frame(
+  x=x3, xend=x3, y=dd$y[x3], yend=rep(paa_points$paa[3],length(x3)))
+p3=p3+geom_segment(data=df,aes(x=x,y=y,yend=yend,xend=xend),col="cyan")
+#
+x4 <- filter(dd, x>breaks[4], x<=breaks[5])$x
+df=data.frame(
+  x=x4, xend=x4, y=dd$y[x4], yend=rep(paa_points$paa[4],length(x4)))
+p3=p3+geom_segment(data=df,aes(x=x,y=y,yend=yend,xend=xend),col="cyan")
+#
+x5 <- filter(dd, x>breaks[5], x<=breaks[6])$x
+df=data.frame(
+  x=x5, xend=x5, y=dd$y[x5], yend=rep(paa_points$paa[5],length(x4)))
+p3=p3+geom_segment(data=df,aes(x=x,y=y,yend=yend,xend=xend),col="cyan")
+p3 = p3 + geom_line(color = "cornflowerblue", lwd=0.7) + 
+  geom_point(data=paa_points, aes(x=paa_centers, y=paa), pch=9, cex=2.7, col="brown") +
+  geom_segment(data=paa_segments, aes(x=x,y=y,xend=xend,yend=yend), color="brown",lwd=1)
+#
+#
+#
+#
+#
+cd <- read.table("../resources/Coffee/Coffee_TEST",as.is=T)[12,-1][70:170]
+dd <- data.frame(x = 1:length(unlist(cd)), y = znorm(unlist(cd)))
+p4 = ggplot(dd, aes(x = x, y = y)) + geom_line(color = "cornflowerblue", lwd=0.7) + 
+  theme_bw() + ggtitle(paste("An example of the Alphabet error computation as the sum\n",
+        "of dist. between PAA and their alphabet cut segment centers")) +
+  scale_x_continuous(limits=c(0,105)) + scale_y_continuous(limits=c(-2,2)) +
+  theme(axis.title=element_blank(), legend.position="none")
+p4
+#
+segments = 5
+inc = length(dd$y)/segments # there shall be 15 segments
+breaks = seq(1-0.5,length(dd$y)+0.5,by = inc)
+p4 + geom_vline(xintercept=breaks, lty=3, color="red")
+#
+paa_points = data.frame(paa = jmotif::paa(dd$y, segments), 
+                paa_centers = (breaks + (inc / 2))[1:segments])
+paa_segments = data.frame(x=breaks[1:segments],y=paa_points$paa,
+                xend=breaks[2:(segments+1)],yend=paa_points$paa,
+                color=c(cbbPalette[4],cbbPalette[6],cbbPalette[7],cbbPalette[7],cbbPalette[4]))
+p4 = p4 + geom_vline(xintercept=breaks, lty=3, color="red") + 
+  geom_point(data=paa_points, aes(x=paa_centers, y=paa), pch=9, cex=2.7, col="brown") +
+  geom_segment(data=paa_segments, aes(x=x,y=y,xend=xend,yend=yend),color=paa_segments$color,lwd=1.2)
+p4
+#
+cuts = data.frame(y=alphabet_to_cuts(3)[2:3])
+cuts_c = data.frame(y=c(-0.967421566101701, 0, 0.967421566101701))
+dseg = data.frame(x=paa_points$paa_centers,xend=paa_points$paa_centers,
+                  y=c(cuts_c$y[3],cuts_c$y[1],cuts_c$y[2],cuts_c$y[2],cuts_c$y[3]), 
+                  yend=paa_points$paa)
+p4 = p4 + geom_hline(data=cuts,aes(yintercept=y), lty=5, lwd=0.8, col="magenta") +
+  geom_hline(data=cuts_c,aes(yintercept=y), lty=2, lwd=0.6, col="magenta") +
+  geom_segment(data=dseg,aes(x=x,y=y,xend=xend,yend=yend),col="cyan",lwd=1.5)
+p4
 
 
-
-Cairo(width = 900, height = 600, 
+Cairo(width = 900, height = 500, 
       file="sax.pdf", type="pdf", pointsize=8, 
-      bg = "transparent", canvas = "white", units = "px", dpi = 74)
-grid.arrange(p,p2,p,p2,ncol=2)
+      bg = "transparent", canvas = "white", units = "px", dpi = 70)
+grid.arrange(p,p3,p2,p4,ncol=2)
 dev.off()
---
+++ src/RCode/IDEA-figures/discrtization_approximation.R
@@ -0,0 +1,203 @@
+require(reshape)
+require(plyr)
+require(dplyr)
+#
+require(stringr)
+#
+require(lattice)
+require(ggplot2)
+require(gridExtra)
+require(scales)
+require(Cairo)
+# The palette with grey:
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", 
+               "#D55E00", "#CC79A7")
+# The palette with black:
+cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", 
+                "#D55E00", "#CC79A7")
+# To use for fills, add
+# scale_fill_manual(values=cbPalette)
+# To use for line and point colors, add
+# scale_colour_manual(values=cbPalette)
+#
+data = read.table("../resources/IDEA-sampling/sampler_repair.out", as.is = T, header = T)
+data[data$frequency == -2147483648,]$frequency = NA
+data = data[complete.cases(data),]
+data$reduction = data$pruned_rules / data$rules
+data = filter(data, cover > 0.95, approximation < 50)
+hist(data$approximation)
+#
+head(arrange(data, reduction))
+datasets = unique(data$dataset)
+summary = ddply(data, .(dataset), function(x){
+  mv = min(x$reduction)
+  x[x$reduction==mv,]
+})
+write.table(summary, "tmp.csv",col.names=T,quote=T,row.names=F,sep=",")
+
+
+dd=read.table("../resources/test-data/stdb_308.txt")
+plot(dd$V1,type="l")
+#
+str(data)
+range(data[data$dataset=="dutch_power_demand",]$window)
+
+dfp = data[!(data$dataset %in% c("gps_track")),]
+dfp = rbind(dfp, data[(data$dataset %in% c("gps_track")) & data$window<700 & data$window>60,])
+pl = dlply(data, .(dataset), function(x){
+ wireframe(approximation ~ paa * alphabet, data = x,
+          xlab = "PAA", 
+          ylab = "Alphabet",
+          zlab = list(label="Approx. error",rot=90),
+          main = paste(unique(x$dataset)),
+          #par.settings = list(#box.3d = list(col = "transparent", alpha = 0),
+          #axis.line = list(col = "transparent"),
+          par.settings = list(clip=list(panel=FALSE)),
+          drape = TRUE,
+          colorkey = TRUE,
+          col.regions = rainbow(100, s = 1, v = 1, start = 0, end = max(1,100 - 1)/100, alpha = 1),
+          screen = list(z = -120, x = -70)
+)})
+Cairo(width = 900, height = 700, 
+      file="approximation_parameters.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+grid.arrange(pl[[1]], pl[[5]], pl[[7]],
+             pl[[3]], pl[[8]], pl[[10]], 
+             ncol=3, nrow=2)
+dev.off()
+
+# correlation between approximation and the rules total
+pl = dlply(data, .(dataset), function(x){
+  cor <- round(cor(x$approximation, x$rules), 4)
+  pv <- round(cor.test(x$approximation, x$rules)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  mr <- min(x$reduction)
+  dd <- data.frame(x=x[x$approximation==mr,]$rules[1],y=mr)
+  p <- ggplot(x, aes(approximation, rules)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous("Approximation error", limits = c(0,max(x$approximation))) + 
+    scale_y_continuous("Number of rules in a grammar") +
+    geom_text(data = NULL, label = ll, x = 0.1, y = 0.34, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+})  
+#do.call(grid.arrange,  pl)
+
+Cairo(width = 900, height = 450, 
+      file="approximation_size_corr.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 70)
+grid.arrange(pl[[1]], pl[[5]], pl[[7]],
+             pl[[3]], pl[[8]], pl[[10]], 
+             ncol=3, nrow=2)
+dev.off()
+
+
+# correlation between frequency and the reduction
+pl = dlply(data, .(dataset), function(x){
+  cor <- round(cor(x$pruned_frequency, x$reduction), 4)
+  pv <- round(cor.test(x$pruned_frequency, x$reduction)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  mr <- min(x$reduction)
+  dd <- data.frame(x=x[x$reduction==mr,]$pruned_frequency[1],y=mr)
+  p <- ggplot(x, aes(pruned_frequency, reduction)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous("most frequent rule occurrence",limits = c(0,max(x$pruned_frequency))) + 
+    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
+    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
+})  
+#do.call(grid.arrange,  pl)
+
+Cairo(width = 900, height = 450, 
+      file="reduction_frequency_corr.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+grid.arrange(pl[[1]], pl[[5]], pl[[7]],
+             pl[[3]], pl[[8]], pl[[10]], 
+             ncol=3, nrow=2)
+dev.off()
+
+# correlation between frequency and the reduction
+pl = dlply(data, .(dataset), function(x){
+  cor <- round(cor(x$pruned_frequency, x$reduction), 4)
+  pv <- round(cor.test(x$pruned_frequency, x$reduction)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  mr <- min(x$reduction)
+  dd <- data.frame(x=x[x$reduction==mr,]$pruned_frequency[1],y=mr)
+  p <- ggplot(x, aes(pruned_frequency, reduction)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous("", limits = c(0,max(x$pruned_frequency))) + 
+    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
+    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
+})  
+#do.call(grid.arrange,  pl)
+
+Cairo(width = 900, height = 600, 
+      file="reduction_frequency_corr.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+grid.arrange(pl[[1]], pl[[5]], pl[[7]],
+             pl[[3]], pl[[8]], pl[[10]], 
+             ncol=3, nrow=2)
+dev.off()
+
+data = data[data$approximation<2,]
+# correlation between approximation and the rules total
+pl = dlply(data, .(dataset), function(x){
+  if(!is.na(str_match(paste(unique(x$dataset)), "TEK"))){
+    x = x[x$pruned_frequency<30,]
+  }
+  cor <- round(cor(x$approximation, x$rules), 4)
+  pv <- round(cor.test(x$approximation, x$rules)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  mr <- min(x$reduction)
+  dd <- data.frame(x=x[x$approximation==mr,]$rules[1],y=mr)
+  p <- ggplot(x, aes(approximation, rules)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous(limits = c(0,max(x$approximation))) + 
+    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
+})  
+#do.call(grid.arrange,  pl)
+
+Cairo(width = 900, height = 500, 
+      file="approximation_frequency_corr.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+grid.arrange(pl[[1]], pl[[5]], pl[[7]],
+             pl[[3]], pl[[8]], pl[[10]], 
+             ncol=3, nrow=2)
+dev.off()
+
+dd=data[data$dataset=="gps_track",]
+plot(dd$rules,dd$approximation)
+
+
+#
+# correlation between approximation and the reduction
+pl = dlply(data, .(dataset), function(x){
+  cor <- round(cor(x$approximation, x$reduction), 4)
+  pv <- round(cor.test(x$approximation, x$reduction)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  print(ll)
+  p <- ggplot(x, aes(approximation, reduction)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous(limits = c(0.2,1.25), breaks = c(0.25, 0.5, 0.75, 1.0, 1.25)) + 
+    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
+    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+  p
+})  
+do.call(grid.arrange,  pl)
+
+Cairo(width = 900, height = 600, 
+      file="approx_reduction_corr.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+do.call(grid.arrange,  pl)
+dev.off()
+
