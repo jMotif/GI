2261bbec82df91fdf07f559604816680fb222c4b
seninp <seninp@gmail.com>
2015-12-10 15:12:20 +0100

    working on grammars, breaking the build

--- src/RCode/IDEA-figures/discrtization_approximation.R
+++ src/RCode/IDEA-figures/discrtization_approximation.R
@@ -38,229 +38,48 @@ df_repair = data.frame(select(data_r,dataset,window,paa,alphabet,rules,frequency
 #
 df_common = inner_join(df_repair, df_sequitur, by = c("dataset","window","paa","alphabet"))
 unique(df_common$dataset)
+range(df_common$window)
+range(df_common$paa)
+range(df_common$alphabet)
+
 #
 df = select(filter(df_common, dataset == "ecg0606"), algorithm.x, frequency.x, cover.x)
 setnames(df, c("algorithm.y","frequency.y","cover.y"))
 df = rbind(df, select(df_common,algorithm.y,frequency.y,cover.y))
 setnames(df, c("algorithm","frequency","cover"))
-ggplot(df[df$cover>0.98 & df$frequency < 100,], aes(x = frequency, fill=algorithm)) + 
-  geom_density(alpha=0.5, binwidth=1) + geom_vline(xintercept=14, col="red") + theme_bw() +
+ecg0606_density <- ggplot(df[df$cover>0.98 & df$frequency < 100,], aes(x = frequency, fill=algorithm)) + 
+  geom_density(alpha=0.5, binwidth=1) + theme_bw() +
   ggtitle(paste("Estimated kernel densities for the most frequent rule occurrence\n",
-    "when the total cover above 0.98"))
+    "in ECG0606 when the total cover is above 0.98")) +
+  scale_x_continuous(limits=c(0,50),breaks=seq(0,50,by=10)) +
+  theme(legend.position="bottom",legend.direction="horizontal")
+#
+dd = read.table("../resources/test-data/ecg0606.txt")
+df_ecg = data.frame(x=c(1:length(dd$V1)), y=dd$V1)
+ecg0606_plot <- ggplot(data=df_ecg, aes(x=x,y=y)) + geom_line(col=cbbPalette[6]) +
+  theme_bw() + ggtitle("Dataset ECG0606 with abnormal heartbeat highlighted") + 
+  geom_path(data=df_ecg[411:511,], col="red")
 
-  
-df = select(filter(df_common, dataset=="stdb_308"), algorithm.x, frequency.x)
-setnames(df, c("algorithm.y","frequency.y"))
-df = rbind(df, select(df_common,algorithm.y,frequency.y))
-setnames(df, c("algorithm","frequency"))
-ggplot(df[df$frequency<50,], aes(x = frequency, fill=algorithm)) + geom_density(alpha=0.5)
+df = select(filter(df_common, dataset == "ann_gun_CentroidA1"), algorithm.x, frequency.x, cover.x)
+setnames(df, c("algorithm.y","frequency.y","cover.y"))
+df = rbind(df, select(df_common,algorithm.y,frequency.y,cover.y))
+setnames(df, c("algorithm","frequency","cover"))
+video_density <- ggplot(df[df$cover>0.98 & df$frequency < 150,], aes(x = frequency, fill=algorithm)) + 
+  geom_density(alpha=0.5, binwidth=1) + theme_bw() +
+  ggtitle(paste("Estimated kernel densities for the most frequent rule occurrence\n",
+                "in Video dataset when the total cover is above 0.98")) +
+  scale_x_continuous(limits=c(0,150),breaks=seq(0,150,by=25)) +
+  theme(legend.position="bottom",legend.direction="horizontal")
+#
+dd = read.table("../resources/test-data/ann_gun_CentroidA1.txt")
+df_video = data.frame(x=c(1:length(dd$V1)), y=dd$V1)
+video_plot <- ggplot(data=df_video, aes(x=x,y=y)) + geom_line(col=cbbPalette[6]) +
+  theme_bw() + ggtitle("Video dataset (~70 normal cycles)") +
+  scale_y_continuous(limits=c(180,500)) 
 
-
-df = select(filter(df_common, dataset=="ann_gun_CentroidA1"), algorithm.x, frequency.x)
-setnames(df, c("algorithm.y","frequency.y"))
-df = rbind(df, select(df_common,algorithm.y,frequency.y))
-setnames(df, c("algorithm","frequency"))
-ggplot(df[df$frequency<100,], aes(x = frequency, fill=algorithm)) + geom_density(alpha=0.5)
-
-df = select(filter(df_common, dataset=="ann_gun_CentroidA1"), algorithm.x, frequency.x)
-setnames(df, c("algorithm.y","frequency.y"))
-df = rbind(df, select(df_common,algorithm.y,frequency.y))
-setnames(df, c("algorithm","frequency"))
-ggplot(df[df$frequency<100,], aes(x = frequency, fill=algorithm)) + geom_density(alpha=0.5)
-
-
-data[data$frequency == -2147483648,]$frequency = NA
-data = data[complete.cases(data),]
-data$reduction = data$pruned_rules / data$rules
-data = filter(data, cover > 0.95, approximation < 50)
-hist(data$approximation)
-#
-head(arrange(data, reduction))
-datasets = unique(data$dataset)
-#data=filter(data,paa>3,paa<=15,alphabet>=3,alphabet<=10)
-summary = ddply(data, .(dataset), function(x){
-  mv = min(x$reduction)
-  x[x$reduction==mv,]
-})
-write.table(summary, "tmp.csv",col.names=T,quote=T,row.names=F,sep=",")
-#
-#
-#
-#
-#
-#
-dfp = data[!(data$dataset %in% c("gps_track")),]
-dfp = rbind(dfp, data[(data$dataset %in% c("gps_track")) & data$window<700 & data$window>60,])
-pl = dlply(data, .(dataset), function(x){
-  show=FALSE
-  #if(unique(x$dataset)=="gps_track" | unique(x$dataset)=="nprs43"){
-  #  show=TRUE
-  #}
- wireframe(approximation ~ paa * alphabet, data = x,
-          xlab = "PAA", 
-          ylab = "Alphabet",
-          zlab = list(label="Approx. error",rot=90),
-          main = paste(unique(x$dataset)),
-          #par.settings = list(#box.3d = list(col = "transparent", alpha = 0),
-          #axis.line = list(col = "transparent"),
-          par.settings = list(clip=list(panel=FALSE)),
-          drape = TRUE,
-          colorkey = show,
-          col.regions = rainbow(100, s = 1, v = 1, start = 0, end = max(1,100 - 1)/100, alpha = 1),
-          screen = list(z = -120, x = -70)
-)})
-Cairo(width = 900, height = 700, 
-      file="approximation_parameters.pdf", type="pdf", pointsize=8, 
+Cairo(width = 1100, height = 400, 
+      file="densities.pdf", type="pdf", pointsize=8, 
       bg = "transparent", canvas = "white", units = "px", dpi = 74)
-grid.arrange(pl[[1]], pl[[5]], pl[[7]],
-             pl[[3]], pl[[8]], pl[[10]], 
-             ncol=3, nrow=2)
+grid.arrange(ecg0606_plot,video_plot,ecg0606_density, video_density,ncol=2,
+             heights=c(2/5, 3/5))
 dev.off()
-#
-#
-#
-#
-#
-#
-# correlation between approximation and the rules total
-pl = dlply(data, .(dataset), function(x){
-  cor <- round(cor(x$approximation, x$rules), 4)
-  pv <- round(cor.test(x$approximation, x$rules)$"p.value", 4)
-  ll <- paste("r=", cor, "\npval=", pv, sep = "")
-  mr <- min(x$reduction)
-  dd <- data.frame(x=x[x$approximation==mr,]$rules[1],y=mr)
-  #df = x[!duplicated(x[,c(5,6)]), ]
-  p <- ggplot(x, aes(approximation, rules)) + geom_smooth(method = "lm") + 
-    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
-    theme_bw() + ggtitle(paste(unique(x$dataset))) +
-    scale_x_continuous("Approximation error", limits = c(0,max(x$approximation))) + 
-    scale_y_continuous("Number of rules in a grammar") +
-    geom_text(data = NULL, label = ll, x = 0.1, y = 0.34, col = cbPalette[6], 
-              hjust = 0, vjust = 0)
-})  
-#do.call(grid.arrange,  pl)
-
-Cairo(width = 900, height = 450, 
-      file="approximation_size_correlation.png", type="png", pointsize=8, 
-      bg = "transparent", canvas = "white", units = "px", dpi = 70)
-grid.arrange(pl[[1]], pl[[5]], pl[[7]],
-             pl[[3]], pl[[8]], pl[[10]], 
-             ncol=3, nrow=2)
-dev.off()
-
-
-# correlation between frequency and the reduction
-pl = dlply(data, .(dataset), function(x){
-  cor <- round(cor(x$pruned_frequency, x$reduction), 4)
-  pv <- round(cor.test(x$pruned_frequency, x$reduction)$"p.value", 4)
-  ll <- paste("r=", cor, "\npval=", pv, sep = "")
-  mr <- min(x$reduction)
-  dd <- data.frame(x=x[x$reduction==mr,]$pruned_frequency[1],y=mr)
-  #df = x[!duplicated(x[,c(13,16)]), ]
-  p <- ggplot(x, aes(pruned_frequency, reduction)) + geom_smooth(method = "lm") + 
-    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
-    theme_bw() + ggtitle(paste(unique(x$dataset))) +
-    scale_x_continuous("most frequent rule occurrence",limits = c(0,max(x$pruned_frequency))) + 
-    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
-    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
-              hjust = 0, vjust = 0)
-  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
-})  
-#do.call(grid.arrange,  pl)
-
-Cairo(width = 900, height = 450, 
-      file="reduction_frequency_correlation.png", type="png", pointsize=8, 
-      bg = "transparent", canvas = "white", units = "px", dpi = 74)
-grid.arrange(pl[[1]], pl[[5]], pl[[7]],
-             pl[[3]], pl[[8]], pl[[10]], 
-             ncol=3, nrow=2)
-dev.off()
-
-# correlation between frequency and the reduction
-pl = dlply(data, .(dataset), function(x){
-  cor <- round(cor(x$pruned_frequency, x$reduction), 4)
-  pv <- round(cor.test(x$pruned_frequency, x$reduction)$"p.value", 4)
-  ll <- paste("r=", cor, "\npval=", pv, sep = "")
-  mr <- min(x$reduction)
-  dd <- data.frame(x=x[x$reduction==mr,]$pruned_frequency[1],y=mr)
-  p <- ggplot(x, aes(pruned_frequency, reduction)) + geom_smooth(method = "lm") + 
-    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
-    theme_bw() + ggtitle(paste(unique(x$dataset))) +
-    scale_x_continuous("", limits = c(0,max(x$pruned_frequency))) + 
-    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
-    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
-              hjust = 0, vjust = 0)
-  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
-})  
-#do.call(grid.arrange,  pl)
-
-Cairo(width = 900, height = 500, 
-      file="reduction_frequency_correlation.pdf", type="pdf", pointsize=8, 
-      bg = "transparent", canvas = "white", units = "px", dpi = 74)
-grid.arrange(pl[[1]], pl[[5]], pl[[7]],
-             pl[[3]], pl[[8]], pl[[10]], 
-             ncol=3, nrow=2)
-dev.off()
-
-data = data[data$approximation<2,]
-# correlation between approximation and the rules total
-pl = dlply(data, .(dataset), function(x){
-  if(!is.na(str_match(paste(unique(x$dataset)), "TEK"))){
-    x = x[x$pruned_frequency<30,]
-  }
-  cor <- round(cor(x$approximation, x$rules), 4)
-  pv <- round(cor.test(x$approximation, x$rules)$"p.value", 4)
-  ll <- paste("r=", cor, "\npval=", pv, sep = "")
-  mr <- min(x$reduction)
-  dd <- data.frame(x=x[x$approximation==mr,]$rules[1],y=mr)
-  p <- ggplot(x, aes(approximation, rules)) + geom_smooth(method = "lm") + 
-    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
-    theme_bw() + ggtitle(paste(unique(x$dataset))) +
-    scale_x_continuous(limits = c(0,max(x$approximation))) + 
-    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
-              hjust = 0, vjust = 0)
-  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
-})  
-#do.call(grid.arrange,  pl)
-
-Cairo(width = 900, height = 500, 
-      file="approximation_frequency_correlation.pdf", type="pdf", pointsize=8, 
-      bg = "transparent", canvas = "white", units = "px", dpi = 74)
-grid.arrange(pl[[1]], pl[[5]], pl[[7]],
-             pl[[3]], pl[[8]], pl[[10]], 
-             ncol=3, nrow=2)
-dev.off()
-
-dd=data[data$dataset=="gps_track",]
-plot(dd$rules,dd$approximation)
-
-
-#
-# correlation between approximation and the reduction
-pl = dlply(data, .(dataset), function(x){
-  cor <- round(cor(x$approximation, x$reduction), 4)
-  pv <- round(cor.test(x$approximation, x$reduction)$"p.value", 4)
-  ll <- paste("r=", cor, "\npval=", pv, sep = "")
-  print(ll)
-  mr <- min(x$reduction)
-  dd <- data.frame(xv=x[x$reduction==mr,]$approximation[1],yv=mr)
-  print(paste(dd))
-  p <- ggplot(x, aes(approximation, reduction)) + geom_smooth(method = "lm") + 
-    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
-    theme_bw() + ggtitle(paste(unique(x$dataset))) +
-    scale_x_continuous(limits = c(0.2,1.25), breaks = c(0.25, 0.5, 0.75, 1.0, 1.25)) + 
-    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
-    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
-              hjust = 0, vjust = 0)
-  p + geom_point(data=dd, aes(x=xv, y=yv), color="red", size=3.5)
-})  
-#do.call(grid.arrange,  pl)
-
-Cairo(width = 900, height = 500, 
-      file="approximation_reduction_correlation.pdf", type="pdf", pointsize=8, 
-      bg = "transparent", canvas = "white", units = "px", dpi = 74)
-grid.arrange(pl[[1]], pl[[5]], pl[[7]],
-             pl[[3]], pl[[8]], pl[[10]], 
-             ncol=3, nrow=2)
-dev.off()
-
