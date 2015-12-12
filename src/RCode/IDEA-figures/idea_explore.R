da8a523eb68992091731092289bcee3817e7e4eb
seninp <seninp@gmail.com>
2015-12-12 13:44:29 +0100

    updating the figures

--- src/RCode/IDEA-figures/idea_explore.R
+++ src/RCode/IDEA-figures/idea_explore.R
@@ -1,6 +1,7 @@
 require(reshape)
 require(plyr)
 require(dplyr)
+require(data.table)
 #
 require(stringr)
 #
@@ -20,28 +21,136 @@ cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2"
 # To use for line and point colors, add
 # scale_colour_manual(values=cbPalette)
 #
-datas = read.table("../resources/sampling/ecg0606_grammarsampler.out", as.is = T, header = T)
-datas[datas$frequency == -2147483648,]$frequency = NA
-datas = datas[complete.cases(datas),]
-datas = filter(datas, cover > 0.95)
-datas$reduction = datas$pruned_rules / datas$rules
-head(arrange(datas, reduction))
-
-
-datar = read.table("../resources/sampling/ecg0606_repair_grammarsampler.out", as.is = T, header = T)
-datar[datar$frequency == -2147483648,]$frequency = NA
-datar = datar[complete.cases(datar),]
-datar = filter(datar, cover > 0.95)
-datar$reduction = datar$pruned_rules / datar$rules
-head(arrange(datar, reduction))
-
-
+data_r = fread(input = "zcat ../resources/IDEA-sampling/sampler_repair.out.gz")
+data_r[data_r$frequency == -2147483648,]$frequency = 0
+data_r = data_r[complete.cases(data_r),]
+data=data_r
 
 datasets = unique(data$dataset)
 datasets
 #
 data$reduction = data$pruned_rules / data$rules
 str(data)
+
+data = data[data$dataset %in% c(
+  "300_signal1", "dutch_power_demand", "gps_track", "ann_gun_CentroidA1", "insect", "nprs43"
+),]
+data[data$dataset=="300_signal1",]$dataset = "ECG300"
+data[data$dataset=="dutch_power_demand",]$dataset = "Dutch Power Demand"
+data[data$dataset=="gps_track",]$dataset = "GPS Track"
+data[data$dataset=="ann_gun_CentroidA1",]$dataset = "Video telemetry"
+data[data$dataset=="insect",]$dataset = "Insect"
+data[data$dataset=="nprs43",]$dataset = "NPRS43"
+unique(data$dataset)
+data = filter(data, cover > 0.95, approximation < 50)
+# correlation between approximation and the rules total
+pl = dlply(data, .(dataset), function(x){
+  cor <- round(cor(x$approximation, x$rules), 4)
+  pv <- round(cor.test(x$approximation, x$rules)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  mr <- min(x$reduction)
+  dd <- data.frame(x=x[x$approximation==mr,]$rules[1],y=mr)
+  p <- ggplot(x, aes(approximation, rules)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous("Approximation error", limits = c(0,max(x$approximation))) + 
+    scale_y_continuous("Number of rules in a grammar") +
+    geom_text(data = NULL, label = ll, x = 0.1, y = 0.34, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+})  
+
+Cairo(width = 900, height = 450, 
+      file="approximation_size_correlation.png", type="png", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 70)
+grid.arrange(pl[[2]], pl[[1]],pl[[3]],
+             pl[[6]],pl[[4]],pl[[5]], ncol=3, nrow=2)
+dev.off()
+
+Cairo(width = 900, height = 450, 
+      file="approximation_size_correlation.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+grid.arrange(pl[[2]], pl[[1]],pl[[3]],
+             pl[[6]],pl[[4]],pl[[5]], ncol=3, nrow=2)
+dev.off()
+#
+#
+#
+# correlation between frequency and the reduction
+pl = dlply(data, .(dataset), function(x){
+  cor <- round(cor(x$pruned_frequency, x$reduction), 4)
+  pv <- round(cor.test(x$pruned_frequency, x$reduction)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  mr <- min(x$reduction)
+  dd <- data.frame(x=x[x$reduction==mr,]$pruned_frequency[1],y=mr)
+  p <- ggplot(x, aes(pruned_frequency, reduction)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous("", limits = c(0,max(x$pruned_frequency))) + 
+    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
+    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
+})  
+#do.call(grid.arrange,  pl)
+
+Cairo(width = 900, height = 450, 
+      file="reduction_frequency_correlation.png", type="png", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 70)
+grid.arrange(pl[[2]], pl[[1]],pl[[3]],
+             pl[[6]],pl[[4]],pl[[5]], ncol=3, nrow=2)
+dev.off()
+
+Cairo(width = 900, height = 450, 
+      file="reduction_frequency_correlation.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+grid.arrange(pl[[2]], pl[[1]],pl[[3]],
+             pl[[6]],pl[[4]],pl[[5]], ncol=3, nrow=2)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+# correlation between frequency and the reduction
+pl = dlply(data, .(dataset), function(x){
+  if(!is.na(str_match(paste(unique(x$dataset)), "TEK"))){
+    x = x[x$pruned_frequency<30,]
+  }
+  cor <- round(cor(x$pruned_frequency, x$reduction), 4)
+  pv <- round(cor.test(x$pruned_frequency, x$reduction)$"p.value", 4)
+  ll <- paste("r=", cor, "\npval=", pv, sep = "")
+  mr <- min(x$reduction)
+  dd <- data.frame(x=x[x$reduction==mr,]$pruned_frequency[1],y=mr)
+  p <- ggplot(x, aes(pruned_frequency, reduction)) + geom_smooth(method = "lm") + 
+    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
+    theme_bw() + ggtitle(paste(unique(x$dataset))) +
+    scale_x_continuous(limits = c(0,max(x$pruned_frequency))) + 
+    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
+    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
+              hjust = 0, vjust = 0)
+  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
+})  
+do.call(grid.arrange,  pl)
+
+Cairo(width = 900, height = 700, 
+      file="reduction_frequency_corr.pdf", type="pdf", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+do.call(grid.arrange,  pl)
+dev.off()
+Cairo(width = 900, height = 700, 
+      file="reduction_frequency_corr.png", type="png", pointsize=8, 
+      bg = "transparent", canvas = "white", units = "px", dpi = 74)
+do.call(grid.arrange,  pl)
+dev.off()
+
+
 #
 # correlation between approximation and the reduction
 pl = dlply(data, .(dataset), function(x){
@@ -87,35 +196,6 @@ Cairo(width = 900, height = 700,
 grid.arrange(pl[[4]], pl[[5]], pl[[7]],
              pl[[3]], pl[[8]], pl[[10]], 
              ncol=3, nrow=2)
-dev.off()
-
-
-
-# correlation between frequency and the reduction
-pl = dlply(data, .(dataset), function(x){
-  if(!is.na(str_match(paste(unique(x$dataset)), "TEK"))){
-    x = x[x$pruned_frequency<30,]
-  }
-  cor <- round(cor(x$pruned_frequency, x$reduction), 4)
-  pv <- round(cor.test(x$pruned_frequency, x$reduction)$"p.value", 4)
-  ll <- paste("r=", cor, "\npval=", pv, sep = "")
-  mr <- min(x$reduction)
-  dd <- data.frame(x=x[x$reduction==mr,]$pruned_frequency[1],y=mr)
-  p <- ggplot(x, aes(pruned_frequency, reduction)) + geom_smooth(method = "lm") + 
-    geom_jitter(alpha = 0.4, col = cbPalette[4], size = 0.35) + 
-    theme_bw() + ggtitle(paste(unique(x$dataset))) +
-    scale_x_continuous(limits = c(0,max(x$pruned_frequency))) + 
-    scale_y_continuous(limits = c(0,1), breaks = c(0, 0.25, 0.5, 0.75, 1.0)) + 
-    geom_text(data = NULL, label = ll, x = 0.25, y = 0.64, col = cbPalette[6], 
-              hjust = 0, vjust = 0)
-  p + geom_point(data=dd, aes(x=x, y=y), color="red", size=3.5)
-})  
-do.call(grid.arrange,  pl)
-
-Cairo(width = 900, height = 700, 
-      file="reduction_frequency_corr.pdf", type="pdf", pointsize=8, 
-      bg = "transparent", canvas = "white", units = "px", dpi = 74)
-do.call(grid.arrange,  pl)
 dev.off()
 
 # correlation between frequency and the reduction
