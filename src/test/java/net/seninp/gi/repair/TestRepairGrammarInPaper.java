82e72e37ff7c1bfa693f76f55000e52c90cb130f
seninp <seninp@gmail.com>
2015-12-13 22:57:16 +0100

    grammar check

--- src/test/java/net/seninp/gi/repair/TestRepairGrammarInPaper.java
+++ src/test/java/net/seninp/gi/repair/TestRepairGrammarInPaper.java
@@ -4,19 +4,27 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import net.seninp.gi.logic.GrammarRuleRecord;
+import net.seninp.gi.logic.GrammarRules;
+import net.seninp.gi.sequitur.SAXRule;
+import net.seninp.gi.sequitur.SequiturFactory;
 
 public class TestRepairGrammarInPaper {
 
-  private static final String TEST_STRING = "abc abc cba XXX abc abc cba";
+  private static final String TEST_STRING = "abc abc cba cba bac XXX abc abc cba cba bac";
 
   private static final String TEST_R0 = "R2 XXX R2";
 
   @Test
-  public void test() {
+  public void test() throws Exception {
 
     RePairGrammar rg = RePairFactory.buildGrammar(TEST_STRING);
     assertNotNull("grammar exists", rg);
+    
+    System.out.println(rg.toGrammarRules() + "\n\n----\n\n");
 
+    SAXRule sr = SequiturFactory.runSequitur(TEST_STRING);
+    System.out.println(SAXRule.printRules());
+    
     GrammarRuleRecord r0 = rg.toGrammarRulesData().get(0);
 
     assertTrue("assert proper grammar", TEST_R0.equals(r0.getRuleString().trim()));
