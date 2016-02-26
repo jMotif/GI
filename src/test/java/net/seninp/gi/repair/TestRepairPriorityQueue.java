3828b02beddb88de90ec8c3efb23cbb7879603dd
Pavel Senin <seninp@gmail.com>
2016-02-26 15:46:34 -0700

    lil cleanup

--- src/test/java/net/seninp/gi/repair/TestRepairPriorityQueue.java
+++ src/test/java/net/seninp/gi/repair/TestRepairPriorityQueue.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import java.util.ArrayList;
-import org.junit.Before;
 import org.junit.Test;
 import net.seninp.gi.rpr.RepairDigramRecord;
 import net.seninp.gi.rpr.RepairPriorityQueue;
